14dd474d62803d5625f6364935b5e9249e9e68f5
domagoj.vukovic <off.vukovic@gmail.com>
2022-11-01 16:55:00 +0100

    feat: user mutations

--- src/resolvers/User/User.resolver.ts
+++ src/resolvers/User/User.resolver.ts
@@ -1,14 +1,35 @@
+import orm from '../../shared/orm'
 import type { UserModule } from './generated'
 
 export const resolvers: UserModule.Resolvers = {
     Mutation: {
-        createUser(parent, context, input) {
-            return {
-                email: '1',
-                firstName: '1',
-                id: '1',
-                lastName: '1',
-            }
+        deleteUser: (_, variables) => {
+            return orm.user.delete({
+                where: {
+                    id: variables.input.id,
+                }
+            })
+        },
+        updateUser: (_, variables) => {
+            return orm.user.update({
+                where: {
+                    id: variables.input.id,
+                },
+                data: {
+                    lastName: variables.input.lastName,
+                    email: variables.input.email,
+                    firstName: variables.input.firstName,
+                }
+            })
+        },
+        createUser: (_, variables) => {
+            return orm.user.create({
+                data: {
+                    email: variables.input.email,
+                    firstName: variables.input.firstName,
+                    lastName: variables.input.lastName,
+                }
+            })
         },
     },
 }
