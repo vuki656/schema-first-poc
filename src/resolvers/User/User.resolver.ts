25e8783bbf5c78b1f928405d7bc088fd9db1de7e
domagoj.vukovic <off.vukovic@gmail.com>
2022-11-01 16:07:56 +0100

    feat: working example

--- src/resolvers/User/User.resolver.ts
+++ src/resolvers/User/User.resolver.ts
@@ -1,3 +1,16 @@
-export const resolvers:  = {
-    
+import type { Resolvers } from '../../generated-types/graphql'
+
+export const resolvers: Resolvers = {
+    Mutation: {
+        createUser(a, b, c) {
+            b.input.firstName
+
+            return {
+                email: '1',
+                firstName: '1',
+                id: '1',
+                lastName: '1',
+            }
+        },
+    },
 }
